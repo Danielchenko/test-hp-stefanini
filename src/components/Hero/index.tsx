cdc17ce9d31a4fd2f09e989971281f44f1fe0037
Daniel Carvalho <daniel.carvalho@agenciam3.com>
2026-05-02 11:36:48 -0300

    feat: last ajust styles and hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,12 +3,14 @@ import styles from './styles.module.scss'
 export const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.overlay}>
+     <div className={styles.container}>
+       <div className={styles.overlay}>
         <h1 className={styles.title}>Wizard Characters</h1>
         <p className={styles.subtitle}>
           Explore the magical world and discover powerful wizards
         </p>
       </div>
+     </div>
     </section>
   )
 }
